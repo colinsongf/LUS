1129d5c34163736c3da7d618d3b239a228b0156d
Rodrigo Sestari <rodrigo.sestari@studenti.unitn.it>
2017-04-10 22:29:34 +0200

    update date update report

--- First/code/transducer_UNK.sh
+++ First/code/transducer_UNK.sh
@@ -5,15 +5,7 @@ total=$(cat ../data/POS.counts | wc -l | bc -l)
 prob=$(echo "-l(1/$total)" | bc -l)
 while read pos count
 do	
-	if [ $treshold -le 0 ]
-	then
-		#equal probability for all concept
-		cost=$(echo "-l(1 / $total)" | bc -l)   
-	else
-		#cut-off
-		countUnk=$((countUnk / $total))      
-	    cost=$(echo "-l($countUnk / $count)" | bc -l)
-	fi
+	cost=$(echo "-l(1 / $total)" | bc -l)   
 	echo -e "0\t0\t<unk>\t$pos\t$cost"
 done < ../data/POS.counts
 echo "0"
